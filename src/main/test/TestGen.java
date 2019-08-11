c18a758e94e9a2bc1d70f7cb46c34004cfba3766
sciencestar <1633696870@qq.com>
2019-08-11 23:09:55 +0800

    Java泛型测试代码提交

--- src/main/test/TestGen.java
+++ src/main/test/TestGen.java
@@ -9,5 +9,9 @@ public class TestGen {
         Gen<Integer> num1 = new GenImpl<Integer>();
         Integer num = num1.getObj(23);
         System.out.println(num);
+
+        Gen<String> stringGen = new GenImpl<String>();
+        String name = stringGen.getObj("Linda");
+        System.out.println(name);
     }
 }
